d50be00e1d2643f249061613c26be4f0eb439698
Michael Bachand <bachand.michael@gmail.com>
2017-11-28 22:01:52 -0800

    Simplifying

--- InteractiveExample.playground/Contents.swift
+++ InteractiveExample.playground/Contents.swift
@@ -1,7 +1,7 @@
 import PlaygroundSupport
 import UIKit
 
-let components: [Component] = [
+let components = [
   Component(backgroundColor: #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1), height: 10),
   Component(backgroundColor: #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1), height: 20),
   Component(backgroundColor: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), height: 30),
